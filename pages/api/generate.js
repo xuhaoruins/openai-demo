6cbf6eaedf6510b94ab8016189f28b96f742b9fe
xuhaoruins <xuhaoruins@hotmail.com>
2023-02-12 04:13:13 +0800

    first commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -29,7 +29,7 @@ export default async function (req, res) {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: chatMsg,
-      temperature: 0.,
+      temperature: 0.7,
       max_tokens:1000,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
